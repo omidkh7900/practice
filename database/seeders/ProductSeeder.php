0c99889d29cacac82c184a365adef374286f64e3
ahadli <amirhossein.adli@gmail.com>
2020-09-24 18:44:26 +0330

    adds seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            'title' => 'Project1',
+            'category' => 'PHP PROGRAMMING',
+            'description' => 'This is a very simple project',
+            'picture' => 'sample',
+        ]);
+    }
+}
