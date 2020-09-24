a7e30667303e68aeca1f7ea5ee64fdfb6528c671
mimikyu97 <amirf97@gmail.com>
2020-09-24 17:48:35 +0330

    ui bootstrap layout

---
+++ resources/views/posts/products.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="row">
+    @foreach ($products as $product)
+    <div class="col-3">
+      <img src="/storage/{{ $product->image }}" alt="products">
+      <h4><a href="/procuts/{{$product->id}}">{{$product->title}}</a></h4>
+    </div>
+    @endforeach
+  </div>
+@endsection
