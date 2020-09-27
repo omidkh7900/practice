5f216d9215c2bd115aa4750512492cda7427dd05
omid <omidkh7900@gamil.com>
2020-09-27 18:31:37 +0330

    fix with and height pic in blade

--- resources/views/products/products.blade.php
+++ resources/views/products/products.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="{{route('products.index')}}">hie</a>
+    <a class="d-block text-center" href="{{route('products.index')}}">hie</a>
   <div class="row">
     @foreach ($products as $product)
     <div class="col-3">
-      <img src="/storage/{{ $product->picture }}" alt="products">
-      <h4><a href="/products/{{$product->id}}">{{$product->title}}</a></h4>
+      <img class="text-center" src="/storage/{{ $product->picture }}" alt="products" width="100%" height="85%">
+      <h4 class="text-center"><a href="/products/{{$product->id}}">{{$product->title}}</a></h4>
     </div>
     @endforeach
   </div>
